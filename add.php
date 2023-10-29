6fbddc8ab18c8d30d830b3ea265a6e4475119453
Elmira PETIT <elledevelope@gmail.com>
2023-10-29 14:46:51 +0100

    Created add.php (form)

---
+++ add.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<?php include 'templates/header.php' ?>
+
+<section class="container grey-text">
+
+    <h4 class="center">Add a Pizza</h4>
+
+    <form class="white" action="" method="">
+        <label>Your Email:</label>
+        <input type="text" name="email">
+
+        <label>Pizza Title:</label>
+        <input type="text" name="title">
+
+        <label>Ingredients (comma separeted):</label>
+        <input type="text" name="ingredients">
+
+        <div class="center">
+            <input class="btn brand z-depth-0" type="submit" name="submit" value="submit">
+        </div>
+    </form>
+
+</section>
+
+<?php include 'templates/footer.php' ?>
+
+</html>
