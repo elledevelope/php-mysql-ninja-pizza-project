cc200a51df5e60be7e21ab43defda1561b2a974a
Elmira PETIT <elledevelope@gmail.com>
2023-10-30 20:44:52 +0100

    Filter and errors for title

--- add.php
+++ add.php
@@ -4,7 +4,7 @@
 // ------------------------------------------ GET Method (data is shown in URL):
 
 //Function to check if a form was submitted /  if  a'submit' btn have been pressed :
-if (isset($_GET['submit'])) { 
+if (isset($_GET['submit'])) {
     echo $_GET['email'] . '<br>';
     echo $_GET['title'] . '<br>';
     echo $_GET['ingredients'] . '<br>';
@@ -22,15 +22,25 @@ if (isset($_POST['submit'])) {
         //filter an email
         $email = $_POST['email'];
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) { //if email passes the filter it's gonna return "true", we reverse it to "false" usung `!` symbol. So if this function gets "false" it prints an error:
-            echo 'email must be a valid email adress'; 
+            echo 'email must be a valid email adress';
         };
     };
 
-    // checking if title field is empty:
-    if (empty($_POST['title'])) {
+    // PHP script used to validate the "title" input received from a form submitted via POST method. It checks if the title is not empty and if it consists of only letters and spaces using regular expressions (RegEx). :
+    if (empty($_POST['title'])) { // This line checks if the 'title' field in the POST request is empty. If it is, it means that the form was submitted without a title, and it echoes "A title is required"
         echo 'A title is required <br>';
     } else {
+        //if (!preg_match('/^[a-zA-Z\s]+$/', $title)): This line uses the preg_match function to perform a regular expression pattern match on the $title. 
         $title = $_POST['title'];
+        if (!preg_match('/^[a-zA-Z\s]+$/', $title)) { //Regular Expression (RegEx). The regular expression /^[a-zA-Z\s]+$/ specifies the pattern that the title must match:
+            // ^: Asserts the start of the string.
+            // [a-zA-Z\s]+: Matches one or more characters that are either lowercase letters (a-z), uppercase letters (A-Z), or whitespace characters (spaces and tabs).
+            // $: Asserts the end of the string.
+            echo 'Title must be letters and spaces only';
+            //If the preg_match function returns true, it means that the title contains only letters and spaces as per the defined pattern. If the condition is met, the code continues execution.
+
+            //If the regular expression check fails (i.e., the title contains characters other than letters and spaces), it echoes "Title must be letters and spaces only," indicating that the user needs to provide a valid title.
+        };
     };
 
     // checking if ingredient field is empty:
