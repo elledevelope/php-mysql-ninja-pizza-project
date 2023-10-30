25321f96e53fdc7887333a4b4dcc2934c3e6f12f
Elmira PETIT <elledevelope@gmail.com>
2023-10-30 21:07:12 +0100

    Created $errors['...'] array

--- add.php
+++ add.php
@@ -1,15 +1,11 @@
 <?php
 
-
-// ------------------------------------------ GET Method (data is shown in URL):
-
-//Function to check if a form was submitted /  if  a'submit' btn have been pressed :
-if (isset($_GET['submit'])) {
-    echo $_GET['email'] . '<br>';
-    echo $_GET['title'] . '<br>';
-    echo $_GET['ingredients'] . '<br>';
-};
-
+//creating a variable $errors - associative array with empty values (those will be filled when we call $errors['....'] in IF)
+$errors = [
+ 'email' => '',
+ 'title' => '',
+ 'ingredient' => '',
+];
 
 
 // ------------------------------------------ POST Method (data is NOT shown in URL, data is send straightly to a server): 
@@ -17,39 +13,33 @@ if (isset($_GET['submit'])) {
 if (isset($_POST['submit'])) {
     // checking if email field is empty:
     if (empty($_POST['email'])) {
-        echo 'An email is required <br>'; //so if 'email' is empty we show error msg
+        $errors['email'] = 'An email is required <br>'; //here we call $errors['email'] and atribute a value of a error-text
     } else {
         //filter an email
         $email = $_POST['email'];
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) { //if email passes the filter it's gonna return "true", we reverse it to "false" usung `!` symbol. So if this function gets "false" it prints an error:
-            echo 'email must be a valid email adress';
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) { 
+            $errors['email'] = 'email must be a valid email adress'; //here we call $errors['email'] and atribute a value of a error-text
         };
     };
 
-    // PHP script used to validate the "title" input received from a form submitted via POST method. It checks if the title is not empty and if it consists of only letters and spaces using regular expressions (RegEx). :
-    if (empty($_POST['title'])) { // This line checks if the 'title' field in the POST request is empty. If it is, it means that the form was submitted without a title, and it echoes "A title is required"
-        echo 'A title is required <br>';
+    // checking if title field is empty:
+    if (empty($_POST['title'])) { 
+        $errors['title'] = 'A title is required <br>';
     } else {
-        //if (!preg_match('/^[a-zA-Z\s]+$/', $title)): This line uses the preg_match function to perform a regular expression pattern match on the $title. 
+        //filter title
         $title = $_POST['title'];
-        if (!preg_match('/^[a-zA-Z\s]+$/', $title)) { //Regular Expression (RegEx). The regular expression /^[a-zA-Z\s]+$/ specifies the pattern that the title must match:
-            // ^: Asserts the start of the string.
-            // [a-zA-Z\s]+: Matches one or more characters that are either lowercase letters (a-z), uppercase letters (A-Z), or whitespace characters (spaces and tabs).
-            // $: Asserts the end of the string.
-            echo 'Title must be letters and spaces only';
-            //If the preg_match function returns true, it means that the title contains only letters and spaces as per the defined pattern. If the condition is met, the code continues execution.
-
-            //If the regular expression check fails (i.e., the title contains characters other than letters and spaces), it echoes "Title must be letters and spaces only," indicating that the user needs to provide a valid title.
+        if (!preg_match('/^[a-zA-Z\s]+$/', $title)) { 
+            $errors['title'] = 'Title must be letters and spaces only';
         };
     };
 
     // checking if ingredient field is empty:
     if (empty($_POST['ingredient'])) {
-        echo 'At least one ingredient is required <br>';
+        $errors['ingridient'] = 'At least one ingredient is required <br>';
     } else {
         $ingredient = $_POST['ingredient'];
         if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){ //Regular Expression (RegEx)
-            echo 'Ingredients must be a comma separated list';
+            $errors['ingridient'] = 'Ingredients must be a comma separated list';
         };
     };
 };
