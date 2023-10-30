d22346526a9c0e3bdabf0d4dec99b747791ff678
Elmira PETIT <elledevelope@gmail.com>
2023-10-30 20:32:29 +0100

    Filter email and print error

--- add.php
+++ add.php
@@ -4,7 +4,7 @@
 // ------------------------------------------ GET Method (data is shown in URL):
 
 //Function to check if a form was submitted /  if  a'submit' btn have been pressed :
-if (isset($_GET['submit'])) { //if $_GET['submit'] is not set(=empty = in this case - if a form was not send yet(no data was send)/the btn 'submit' was not pressed) then, when the btn is pressed we echo data from a form:
+if (isset($_GET['submit'])) { 
     echo $_GET['email'] . '<br>';
     echo $_GET['title'] . '<br>';
     echo $_GET['ingredients'] . '<br>';
@@ -15,18 +15,13 @@ if (isset($_GET['submit'])) { //if $_GET['submit'] is not set(=empty = in this c
 // ------------------------------------------ POST Method (data is NOT shown in URL, data is send straightly to a server): 
 // -------------------------------------------POST Method is more secure 
 if (isset($_POST['submit'])) {
-    // !!!!! IMPORTANT : always have to use //-- htmlspecialchars() --// to prevent Cross Site Scripting (XSS) Attaks (in this case via a form method POST ) !!!!!
-
-    // echo htmlspecialchars($_POST['email']) . '<br>';
-    // echo htmlspecialchars($_POST['title']) . '<br>';
-    // echo htmlspecialchars($_POST['ingredients']) . '<br>';
-
     // checking if email field is empty:
     if (empty($_POST['email'])) {
         echo 'An email is required <br>'; //so if 'email' is empty we show error msg
     } else {
+        //filter an email
         $email = $_POST['email'];
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) { //we filter email and if it do don't pass the filter (usung ! here we inverse true to false) we pring an error:
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) { //if email passes the filter it's gonna return "true", we reverse it to "false" usung `!` symbol. So if this function gets "false" it prints an error:
             echo 'email must be a valid email adress'; 
         };
     };
