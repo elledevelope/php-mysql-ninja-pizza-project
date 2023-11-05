95d299c46ce558ee8663ff3b45537418a8deeeca
Elmira PETIT <elledevelope@gmail.com>
2023-11-05 15:58:52 +0100

    Got data from database in assoc array

--- index.php
+++ index.php
@@ -5,6 +5,21 @@
 <?php include 'templates/footer.php' ?>
 <?php include 'models/Database.php' ?>
 
-
-
 </html>
+
+
+
+<?php
+//write query for all pizzas:
+$sql = 'SELECT title, ingredients, id FROM pizzas';
+
+//make query and get result:
+$result = mysqli_query($conn, $sql);
+
+//fetch the resulting rows as an array:
+$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+print_r($pizzas);
+
+
+?>
