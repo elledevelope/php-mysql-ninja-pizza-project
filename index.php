f00e6f67acf40da6f101f931f917df4c87cbce72
Elmira PETIT <elledevelope@gmail.com>
2023-11-05 16:18:38 +0100

    Output data from database to browser - foreach

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="fr">
-
 <?php include 'templates/header.php' ?>
 <?php include 'templates/footer.php' ?>
 <?php include 'models/Database.php' ?>
-
-</html>
-
 
 
 <?php
@@ -26,7 +20,37 @@ mysqli_free_result($result);
 mysqli_close($conn);
 
 
-print_r($pizzas);
-
+// print_r($pizzas);
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<h4 class="center grey-text">
+    Pizzas!
+</h4>
+
+<div class="container">
+    <div class="row">
+            <?php foreach($pizzas as $pizza){ ?>
+
+                <div class="col s6 md3">
+                    <div class="card z-depth-0">
+                        <div class="card-content center">
+                            <h6><?php echo htmlentities($pizza['title']); ?></h6>
+                            <div><?php echo htmlentities($pizza['ingredients']); ?></div>
+                        </div>
+                        <div class="card-action">
+                            <a href="#" class="brand-text">more info</a>
+                        </div>
+                    </div>
+                </div>
+
+            <?php }; ?> 
+
+    </div>
+</div>
+
+</html>
