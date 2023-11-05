1ddca6cd8451406234817f58d181c4f8c69fb63d
Elmira PETIT <elledevelope@gmail.com>
2023-11-05 21:16:27 +0100

    Added delete form in html

--- details.php
+++ details.php
@@ -28,12 +28,20 @@ mysqli_close($conn);
 <?php include 'templates/header.php' ?>
 
 <div class="container center">
+
     <?php if ($pizza) : ?>
         <h4><?php echo htmlspecialchars($pizza['title']); ?></h4>
         <p>Created by: <?php echo htmlspecialchars($pizza['email']); ?></p>
         <p> <?php echo date($pizza['created_at']); ?></p>
         <h5>Ingredients:</h5>
         <p><?php echo htmlspecialchars($pizza['ingredients']); ?></p>
+
+        <!-- DELETE FORM -->
+        <form action="details.php" method="
+        POST">
+            <input type="hidden" name="id_to_delete" value="<?php echo $pizza['id']; ?>">
+            <input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+        </form>
 
     <?php else : ?>
         <h5>No such pizza exists</h5>
