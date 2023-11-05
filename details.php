baedddc78e7a9ec1a3565e57b5e7949527d2afdd
Elmira PETIT <elledevelope@gmail.com>
2023-11-05 17:21:19 +0100

    Created detail.php

---
+++ details.php
@@ -0,0 +1,18 @@
+
+<?php include 'models/Database.php' ?>
+
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<?php include 'templates/header.php' ?>
+
+<h2>Details</h2>
+
+<?php include 'templates/footer.php' ?>
+</html>
+
