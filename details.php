419f5d258310101887ba6150a044d0c379bf6d24
Elmira PETIT <elledevelope@gmail.com>
2023-11-05 17:36:44 +0100

    Getting single record(id) via $sql

--- details.php
+++ details.php
@@ -2,8 +2,15 @@
 <?php include 'models/Database.php' ?>
 
 <?php
+//check GET['id'] (GET request 'id' parameter):
+if(isset($_GET['id'])) { //if 'id' is present in the url
 
+ $id = mysqli_real_escape_string($conn, $_GET['id']);   //escaping any sensitive characters to protect our database
 
+//make sql:
+$sql = "SELECT * FROM pizzas WHERE id = $id"; // for btn "more info" we select any record ` WHERE id = $id ` (id in url)
+
+};
 
 ?>
 
