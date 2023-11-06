1bc16d6d88b3e26f980c456b636ae3660f53e4fb
Elmira PETIT <elledevelope@gmail.com>
2023-11-06 19:54:19 +0100

    Superglobals info

---
+++ sandbox.php
@@ -0,0 +1,26 @@
+<?php 
+//Youtube: https://www.youtube.com/watch?v=sMhSKNAHXZ8&list=PL4cUxeGkcC9gksOX3Kd9KPo-O68ncT05o&index=35
+
+	// superglobals
+
+	//$_GET['name'], $_POST['name']
+
+	echo $_SERVER['SERVER_NAME'] . '<br />';
+	echo $_SERVER['REQUEST_METHOD'] . '<br />';
+	echo $_SERVER['SCRIPT_FILENAME'] . '<br />';
+	echo $_SERVER['PHP_SELF'] . '<br />'; //gets a curent file name
+
+	// $_COOKIE, $_SESSION
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>php tuts</title>
+</head>
+<body>
+
+</body>
+</html>
